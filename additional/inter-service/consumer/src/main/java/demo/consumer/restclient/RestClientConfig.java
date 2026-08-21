7033cad58610953b65ea2b9749979c2340efd29e
22120236 <22120236@student.hcmus.edu.vn>
2026-08-21 10:27:39 +0700

    feat: add Eureka service discovery
    - Add Eureka client dependencies to consumer and producer services
    
    - Register consumer and producer with Eureka at localhost:8761
    
    - Switch Feign, RestClient, and RestTemplate calls to service discovery name producer
    
    - Add new Eureka server modules

--- additional/inter-service/consumer/src/main/java/demo/consumer/restclient/RestClientConfig.java
+++ additional/inter-service/consumer/src/main/java/demo/consumer/restclient/RestClientConfig.java
@@ -1,15 +1,29 @@
 package demo.consumer.restclient;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.web.client.RestClient;
-import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class RestClientConfig {
 
     @Bean
-    public RestClient restClient() {
-        return RestClient.builder().baseUrl("http://localhost:8081").build();
+    @Primary
+    public RestClient.Builder restClientBuilder() {
+        return RestClient.builder();
+    }
+
+    @Bean
+    @LoadBalanced
+    public RestClient.Builder loadBalancedRestClientBuilder() {
+        return RestClient.builder();
+    }
+
+    @Bean
+    public RestClient restClient(@Qualifier("loadBalancedRestClientBuilder") RestClient.Builder clientBuilder) {
+        return clientBuilder.baseUrl("http://producer").build();
     }
 }
